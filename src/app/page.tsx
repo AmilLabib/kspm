642809971a071e4393f79959792dde0aea007cb5
AmilLabib <amilsoerabaja@gmail.com>
2026-05-02 14:59:12 +0700

    add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import Update from "./components/Homepage/Update/Update";
 import Pengurus from "./components/Homepage/Pengurus/Pengurus";
 import News from "./components/Homepage/News/News";
 import Footer from "./components/Common/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   const sectionClass = "max-w-6xl w-full mx-auto px-6 py-12";
   return (
     <>
+      <Analytics />
       <div
         style={{ height: "100vh", display: "flex", flexDirection: "column" }}
       >
